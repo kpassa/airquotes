4e7a4a886b2bec2fb60964f869977787fbc28afa
Kyle Passarelli <kpassa615@gmail.com>
2011-10-29 16:46:27 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TsegEstimates::Application.config.secret_token = 'a17e3e5ba266067c0c2a03f877b3da86a22b1134cb079193d4065792a506ed5c7d7decd9124844b6b434e30428964a0dc346f9818b7caa433bdf1255515a2250'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TsegEstimates::Application.config.session_store :cookie_store, key: '_tseg_estimates_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TsegEstimates::Application.config.session_store :active_record_store
